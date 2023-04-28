d7267c5985b38f7727bcee5776df71724fd1a58a
RG <ForUiPathacc94@proton.me>
2023-04-28 23:17:23 +0300

    update conftest

--- conftest.py
+++ conftest.py
@@ -2,12 +2,6 @@ import allure
 from playwright.sync_api import Page
 import pytest
 
-from pages.cart_page import CartPage
-from pages.login_page import LoginPage
-from pages.main_page import MainPage
-from pages.search_page import SearchPage
-from pages.store_page import StorePage
-
 
 @pytest.fixture(scope="function")
 def get_page(page: Page, request):
@@ -28,26 +22,3 @@ def get_page(page: Page, request):
     page.context.tracing.stop(path="reports/trace.zip")
 
 
-@pytest.fixture()
-def main_page(get_page) -> MainPage:
-    return MainPage(get_page)
-
-
-@pytest.fixture()
-def store_page(get_page) -> StorePage:
-    return StorePage(get_page)
-
-
-@pytest.fixture()
-def login_page(get_page) -> LoginPage:
-    return LoginPage(get_page)
-
-
-@pytest.fixture()
-def cart_page(get_page) -> CartPage:
-    return CartPage(get_page)
-
-
-@pytest.fixture()
-def search_page(get_page) -> SearchPage:
-    return SearchPage(get_page)
