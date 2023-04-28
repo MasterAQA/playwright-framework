0ca1a932ef8e3b738341a5bad8f07ed529ed2b49
RG <ForUiPathacc94@proton.me>
2023-04-28 23:11:49 +0300

    change dotenv settings

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+import os
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+dotenv_path = Path('../.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+
 base_page = "https://www.apple.com/"
 
-apple_username = "fernardmiller443@gmail.com"
-apple_password = "SDA51345sadwq!"
-# number = "(215) 2073157"
+apple_username = os.getenv("APPLE_USERNAME")
+apple_password = os.getenv("APPLE_PASSWORD")
